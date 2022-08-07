904c352983e2d44b1225c7674cca039281f47b2f
pastor <pastor@speedandfunction.com>
2022-08-07 14:18:51 +0300

    implementing changes according to test task 2

--- index.php
+++ index.php
@@ -10,8 +10,9 @@ create table users
         primary key (id),
     constraint users_email
         unique (email)
-);
+) ENGINE=InnoDB;
 */
+
 
 interface UserEmailSenderInterface
 {
@@ -23,17 +24,37 @@ interface UserEmailSenderInterface
      *
      * @throws EmailSendException
      */
-    public function sendEmailChangedNotification(string $oldEmail, string $newEmail): void;
+    public function sendEmailChangedNotification(string $oldEmail, string $newEmail): void;
+}
+
+class UserEmailSenderService implements UserEmailSenderInterface 
+{
+    /**
+     * @param string $oldEmail
+     * @param string $newEmail
+     *
+     * @return void
+     *
+     * @throws EmailSendException
+     */
+    public function sendEmailChangedNotification(string $oldEmail, string $newEmail): void
+    {
+        //do email send logic
+    }
+  
 }
 
 
 class UserEmailChangerService
 {
-    private \PDO $db;
+    private \PDO $db;
+    
+    private \UserEmailSenderService $userEmailSenderService;
  
-    public function __construct(\PDO $db)
+    public function __construct(\PDO $db, \UserEmailSenderService $userEmailSenderService)
     {
-        $this->db = $db;
+      $this->db = $db;
+      $this->userEmailSenderService = $userEmailSenderService;
     }
  
     /**
@@ -42,16 +63,32 @@ class UserEmailChangerService
      *
      * @return void
      *
-     * @throws \PDOException
+     * @throws \PDOException|EmailSendException
      */
-    public function changeEmail(int $userId, string $email): void
-    {
-        $statement = $this->db->prepare('UPDATE users SET email = :email WHERE id = :id');
- 
-        $statement->bindParam(':id', $userId, \PDO::PARAM_INT);
-        $statement->bindParam(':email', $email, \PDO::PARAM_STR);
-        $statement->execute();
-    }
-}
+    public function changeEmail(int $userId, string $email): void
+    {
+      try {
+        $this->db->beginTransaction();
 
+        $statement = $this->db->prepare('SELECT id, email FROM users WHERE id = :id FOR UPDATE'); 
+        $statement->bindParam(':id', $userId, \PDO::PARAM_INT);
+        $statement->execute();
+        $user = $stm->fetch(PDO::FETCH_ASSOC);
+        
+        $statement = $this->db->prepare('UPDATE users SET email = :email WHERE id = :id'); 
+        $statement->bindParam(':id', $userId, \PDO::PARAM_INT);
+        $statement->bindParam(':email', $email, \PDO::PARAM_STR);
+        $statement->execute();
+
+        $this->db->commit();
+      } catch (\PDOException $e) {
+        if ($this->db->inTransaction()) {
+            $this->db->rollBack();
+        }
+        throw $e;
+      }
+      
+      $this->userEmailSenderService->sendEmailChangedNotification($user['email'], $email);      
+    }
+}
 ?>
